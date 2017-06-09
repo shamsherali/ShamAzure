bdbae1d7eb246bac6365bf913f7b23caa3e385ff
Admin <Admin@LENOVO>
2017-06-09 10:55:53 +0530

    Add project files.

---
+++ SAzureApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAzureApp.MvcApplication" Language="C#" %>
---
+++ SAzureApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAzureApp.Startup))]
+namespace SAzureApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
